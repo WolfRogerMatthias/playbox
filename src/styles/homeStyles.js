0a2da87aa044f0521177a9ffb1484c34c117feae
Roger Wolf <rogerwolf@gmx.ch>
2026-01-31 12:06:54 +0100

    [RW-Styling + Main] - Added the styling and the main component structure

---
+++ src/styles/homeStyles.js
@@ -0,0 +1,9 @@
+export const homeStyles = (theme) => ({
+    container: {
+        background: theme.palette.background.default,
+        height: '100vh',
+        width: '100vw',
+        margin: 0,
+        padding: 0,
+    }
+})
